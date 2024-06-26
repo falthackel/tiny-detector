0b0bb91b6b888d6abb1c7af53bf145835cfd5e1f
Farrel Jonathan Vickeldo <farrel.jonathan.fj@gmail.com>
2024-06-27 00:15:43 +0700

    Alter landing page to LoginPage()

--- flutter_tiny_detector/lib/main.dart
+++ flutter_tiny_detector/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'sign_up_page.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpPage(),
+      home: LoginPage(), // Make LoginPage the landing page
     );
   }
 }
