ed713c992c6b3130396c21db195ac690f94d2c8b
Farrel Jonathan Vickeldo <farrel.jonathan.fj@gmail.com>
2024-06-26 23:11:48 +0700

    Change landing page to SignUpPage()

--- flutter_tiny_detector/lib/main.dart
+++ flutter_tiny_detector/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'main_page.dart';
+import 'sign_up_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(userId: 1), // Provide a default userId for testing
+      home: SignUpPage(),
     );
   }
 }
