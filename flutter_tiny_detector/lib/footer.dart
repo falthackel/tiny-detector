c3d7add6d5543a854ed425896dd07f47e48d0277
Farrel Jonathan Vickeldo <farrel.jonathan.fj@gmail.com>
2024-06-29 13:20:12 +0700

    change footer

--- flutter_tiny_detector/lib/footer.dart
+++ flutter_tiny_detector/lib/footer.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.all(8.0),
-      color: const Color.fromARGB(255, 1, 204, 209),
-      child: const Center(
-        child: Text(
-          'Tiny Detector ini dibuat oleh 18320008',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '© 2024 Tiny Detector oleh 18320008',
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 14,
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
