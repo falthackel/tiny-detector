35afd2cce5540afacbada1fe5141d0be2453d46d
Farrel Jonathan Vickeldo <farrel.jonathan.fj@gmail.com>
2024-06-30 02:07:17 +0700

    create logout, use CustomAppbar() and Sidebar

--- flutter_tiny_detector/lib/main_page.dart
+++ flutter_tiny_detector/lib/main_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tiny_detector/footer.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_tiny_detector/api_service.dart';
+import 'package:flutter_tiny_detector/custom_appbar.dart';
+import 'package:flutter_tiny_detector/sidebar.dart';
+import 'footer.dart';
 import 'features_options.dart';
 import 'saved_assessment.dart';
 import 'search_widget.dart';
+import 'login_page.dart';
 
 class MainPage extends StatefulWidget {
   final int userId;
@@ -14,43 +19,55 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final storage = const FlutterSecureStorage();
+  String? name;
+  String? email;
+
+  Future<void> _logout() async {
+    await storage.delete(key: 'jwt_token');
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
+
+  Future<void> fetchAssessor() async {
+    try {
+      String? storedEmail = await storage.read(key: 'email');
+
+      if (storedEmail != null) {
+        Map<String, dynamic> assessor = await ApiService.fetchAssessor(storedEmail);
+        print(assessor);
+
+        setState(() {
+          name = assessor['assessor_name'] as String?;
+          email = assessor['assessor_email'] as String?;
+        });
+      } else {
+        print('No email found in storage');
+      }
+    } catch (e) {
+      print('Failed to fetch assessor: $e');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    fetchAssessor();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 1, 204, 209),
-        title: const SizedBox(
-          height: 40,
-          child: Center(
-            child: Image(
-              image: AssetImage('assets/tiny-detector-white.png'),
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.menu, size: 35, color: Colors.white),
-          onPressed: () {},
-        ),
-        actions: [
-          Container(
-            margin: const EdgeInsets.all(5),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-            ),
-            child: IconButton(
-              icon: const Icon(Icons.person),
-              onPressed: () {},
-            ),
-          ),
-        ],
+      appBar: const CustomAppBar(
+        title: 'Tiny Detector', // Replace with your desired title
+        backgroundColor: Color.fromARGB(255, 1, 204, 209),
+      ),
+      drawer: Sidebar(
+        name: name,
+        email: email,
+        onLogout: _logout,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
