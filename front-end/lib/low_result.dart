30b021b4a0dc8582a578f331eb122933eb5d9dc3
Farrel Jonathan <farrel.jonathan.fj@gmail.com>
2024-07-16 19:21:49 +0700

    add singlechildview

--- front-end/lib/low_result.dart
+++ front-end/lib/low_result.dart
@@ -69,76 +69,78 @@ class _LowResultState extends State<LowResult> {
         email: email, 
         onLogout: _logout
       ),
-      body: Container(
-        margin: const EdgeInsets.all(15),
-        padding: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 1, 204, 209),
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Text(
-              'Hasil:',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Indikasi Rendah',
-              style: TextStyle(color: Colors.white, fontSize: 32),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Berdasarkan jawaban Anda, indikasi autisme pada anak tergolong rendah.',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Anda akan menerima email rincian hasil yang berisi jawaban Anda untuk setiap pertanyaan penilaian.',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Perlu diingat, autisme adalah kondisi perkembangan yang gejalanya bisa muncul seiring pertumbuhan anak. Disarankan untuk terus melakukan penilaian sesuai kelompok umur balita.',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Hasil ini tidak dapat menggantikan penilaian perkembangan secara formal. Jika Anda masih memiliki kekhawatiran tentang perkembangan anak Anda, situs web Tiny Detector menyediakan daftar sumber daya tambahan yang dapat diakses melalui tombol SUMBER di bawah ini.',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 30.0),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle button press (e.g., navigate to resources page)
-                  },
-                  child: const Text('Sumber'),
-                ),
-                const SizedBox(height: 10.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => MainPage(userId: widget.userId)),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF04A0A4),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 1, 204, 209),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                'Hasil:',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Indikasi Rendah',
+                style: TextStyle(color: Colors.white, fontSize: 32),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Berdasarkan jawaban Anda, indikasi autisme pada anak tergolong rendah.',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Anda akan menerima email rincian hasil yang berisi jawaban Anda untuk setiap pertanyaan penilaian.',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Perlu diingat, autisme adalah kondisi perkembangan yang gejalanya bisa muncul seiring pertumbuhan anak. Disarankan untuk terus melakukan penilaian sesuai kelompok umur balita.',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Hasil ini tidak dapat menggantikan penilaian perkembangan secara formal. Jika Anda masih memiliki kekhawatiran tentang perkembangan anak Anda, situs web Tiny Detector menyediakan daftar sumber daya tambahan yang dapat diakses melalui tombol SUMBER di bawah ini.',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 30.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle button press (e.g., navigate to resources page)
+                    },
+                    child: const Text('Sumber'),
                   ),
-                  child: const Text(
-                    'Kembali ke Beranda',
-                    style: TextStyle(
-                      color: Colors.white,
+                  const SizedBox(height: 10.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainPage(userId: widget.userId)),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF04A0A4),
+                    ),
+                    child: const Text(
+                      'Kembali ke Beranda',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
